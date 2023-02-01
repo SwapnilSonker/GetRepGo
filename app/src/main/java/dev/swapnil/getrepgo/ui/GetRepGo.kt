05f5e648393d9f27fbe3d8b1a556b7d4565ad61b
SwapnilSonker <swapnilsonkarcse2019@bbdu.ac.in>
2023-02-01 12:55:34 +0530

    Initial commit

---
+++ app/src/main/java/dev/swapnil/getrepgo/ui/GetRepGo.kt
@@ -0,0 +1,9 @@
+package dev.swapnil.getrepgo.ui
+
+import android.app.Application
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GetRepGo : Application(){
+}
---
+++ app/src/main/java/dev/swapnil/getrepgo/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.swapnil.getrepgo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.swapnil.getrepgo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
